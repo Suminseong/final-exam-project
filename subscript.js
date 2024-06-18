84bd4feea4965d55684587dbdde61790cda3078c
Suminseong <epwyqqu309@gmail.com>
2024-06-19 00:51:02 +0900

    connectiong complete

--- subscript.js
+++ subscript.js
@@ -1,1 +1,17 @@
-const logo = document.getElementById('sub-1-section-1-logo')
+document.getElementById('sub-1-section-1-logo').addEventListener('click', () => {
+    location.href='/index.html';
+})
+
+document.getElementById('footer-content-1-logo').addEventListener('click', () => {
+    location.href='/index.html';
+})
+
+document.getElementById('footer-content-1-SNS-1').addEventListener('click', () => {
+    location.href='https://www.facebook.com/waveparkkor/';
+})
+document.getElementById('footer-content-1-SNS-2').addEventListener('click', () => {
+    location.href='https://www.instagram.com/wavepark__?igsh=bXNuZHgxOGhiZGpx&utm_source=qr';
+})
+document.getElementById('footer-content-1-SNS-3').addEventListener('click', () => {
+    location.href='https://https://x.com/siheung/status/1313652734789218304';
+})
