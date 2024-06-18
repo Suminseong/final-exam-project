44b9bf7bea6593a22ad1ea7cfb94de05c3c91761
arane152 <optimusjun1523@gmail.com>
2024-06-19 01:18:47 +0900

    06190118 sub page scroll bar

--- subscript.js
+++ subscript.js
@@ -15,3 +15,18 @@ document.getElementById('footer-content-1-SNS-2').addEventListener('click', () =
 document.getElementById('footer-content-1-SNS-3').addEventListener('click', () => {
     location.href='https://https://x.com/siheung/status/1313652734789218304';
 })
+
+let isAnimating = false;
+
+window.addEventListener('scroll', function () {
+    if (isAnimating) return;
+
+    let scrollTop = window.scrollY;
+
+    const docuHeight = document.documentElement.scrollHeight;
+    const viewportHeight = window.innerHeight;
+    const scrollableHeight = docuHeight - viewportHeight;
+    const scrollProgress = scrollTop / scrollableHeight;
+    const progressBarWidth = scrollProgress * 200;
+    document.getElementById('scroll-progress-current').style.width = progressBarWidth + 'px';
+})
